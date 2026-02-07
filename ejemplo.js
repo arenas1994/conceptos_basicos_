5e2e95974003dd3c09c3461371c97c849440bd7d
Daniel Arenas <d.arenasd@unitecnica.net>
2026-02-06 20:52:39 -0500

    Trabajo en clase

---
+++ ejemplo.js
@@ -0,0 +1,9 @@
+console.log("hola este es un ejemplo de javascrip.");
+let edad = 30;
+
+let nombre,apellidos;
+nombre = "juan";
+apellidos = "perez";
+const telefono="1234";
+console.log (nombre, apellidos, "tiene", edad, "años y su telefono es", telefono);
+
